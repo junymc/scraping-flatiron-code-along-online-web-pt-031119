f475b39e34e02fa8dc0c645d8d6d8eb1ebadc8ae
Junyoung McArdle <juny.mcardle@gmail.com>
2019-04-18 18:11:36 +0000

    Automatically backed up by Learn

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -1,10 +1,18 @@
+require 'pry'
 require 'nokogiri'
 require 'open-uri'
 
 require_relative './course.rb'
 
 class Scraper
-  
+
+    def get_page
+      doc = Nokogiri::HTML(open("http://learn-co-curriculum.github.io/site-for-scraping/courses"))
+
+      binding.pry
+
+    end
+
     def print_courses
     self.make_courses
     Course.all.each do |course|
@@ -15,8 +23,5 @@ class Scraper
       end
     end
   end
-  
+
 end
-
-
-
